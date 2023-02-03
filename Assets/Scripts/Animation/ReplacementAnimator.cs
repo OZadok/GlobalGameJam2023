ef62063007d527877e983f17284e079a061e9238
syoels <syoels@gmail.com>
2023-02-03 10:12:20 +0200

    hide multiple frames from yam prefab

--- Assets/Scripts/Animation/ReplacementAnimator.cs
+++ Assets/Scripts/Animation/ReplacementAnimator.cs
@@ -117,10 +117,8 @@ namespace Animation
             
             _lastFrameResult = CurrFrame.TurnOn();
             if (controlledTransform != null)
-            {
                 controlledTransform.Translate(_lastFrameResult.offset * Speed, Space.Self);
-                // controlledTransform.transform.localPosition += _lastFrameResult.offset * Speed;
-            }
+            
         }
 
         //TODO: add "bool immediate" argument to enable non-immediate transitions
