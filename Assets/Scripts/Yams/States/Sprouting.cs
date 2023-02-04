7dd387413f1ecf69bc97f87444a1ce5cbc63f7bd
Omer Zadok <OZadok@gmail.com>
2023-02-04 05:30:07 +0200

    Yam spawn variation

--- Assets/Scripts/Yams/States/Sprouting.cs
+++ Assets/Scripts/Yams/States/Sprouting.cs
@@ -1,6 +1,7 @@
 using System;
 using SuperMaxim.Messaging;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Yams
 {
@@ -9,11 +10,14 @@ namespace Yams
         [Serializable]
         public struct SproutingStateSettings
         {
-            public float timeToSprout; 
+            public float timeToSproutMin; 
+            public float timeToSproutMax;
+            public float safeDistanceFromPlayer;
         }
 
         private SproutingStateSettings _settings;
         private float timeSinceSprouted;
+        private float _timeToSprout;
 
         public Sprouting(YamStateManager manager, SproutingStateSettings settings) : base(manager)
         {
@@ -25,18 +29,25 @@ namespace Yams
             timeSinceSprouted = 0f;
             manager.Anim.ChangeAnim("Sprout");
             Messenger.Default.Publish(new YamCreatedEvent());
+            _timeToSprout = Random.Range(_settings.timeToSproutMin, _settings.timeToSproutMax);
         }
 
         public override YamStateName Update()
         {
             timeSinceSprouted += Time.deltaTime;
-            if (timeSinceSprouted >= _settings.timeToSprout)
+            if (timeSinceSprouted >= _timeToSprout && IsFarFromPlayer())
             {
                 timeSinceSprouted = 0f;
                 return YamStateName.Alive;
             }
 
             return YamStateName.Sprouting;
+        }
+
+        private bool IsFarFromPlayer()
+        {
+            return (GameManager.Instance.PlayerTransform.position - manager.transform.position).sqrMagnitude >
+                   _settings.safeDistanceFromPlayer * _settings.safeDistanceFromPlayer;
         }
 
         public override void Exit()
